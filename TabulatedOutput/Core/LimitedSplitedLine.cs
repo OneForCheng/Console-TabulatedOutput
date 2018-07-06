757b09265ecacca8423b8339e6373b8758d16ec3
2871979271 <2871979271@qq.com>
2018-07-06 18:32:07 +0800

    [forcheng] - refactor LimitedSplitedLine class

--- TabulatedOutput/Core/LimitedSplitedLine.cs
+++ TabulatedOutput/Core/LimitedSplitedLine.cs
@@ -7,47 +7,52 @@ namespace TabulatedOutput.Core
     {
         public LimitedSplitedLine(string source, string separator, int limitedWordCount) : base(source, separator)
         {
-            if (limitedWordCount >= 1 && limitedWordCount <= WordCount)
+            if (IsValidRange(limitedWordCount))
             {
-                if (limitedWordCount != WordCount)
-                {
-                    var words = new string[limitedWordCount];
-                    if (limitedWordCount == 1)
-                    {
-                        words[0] = source;
-                    }
-                    else
-                    {
-                        var splits = source.Split(new[] { separator }, StringSplitOptions.None);
-                        for (int i = 0; i < limitedWordCount - 1; i++)
-                        {
-                            words[i] = Words[i];
-                        }
-                        var count = 0;
-                        for (int i = 0; i < splits.Length; i++)
-                        {
-                            if (splits[i] != string.Empty)
-                            {
-                                count++;
-                                if (count == limitedWordCount)
-                                {
-                                    words[limitedWordCount - 1] = string.Join(separator, splits.Skip(i));
-                                    break;
-                                }
-                            }
-
-                        }
-                       
-                    }
-                    Words = words;
-                    WordCount = limitedWordCount;
-                }
+                if (limitedWordCount == WordCount) return;
+                Words = GetLimitedWords(source, separator, limitedWordCount);
+                WordCount = limitedWordCount;
             }
             else
             {
                 Words = new []{source};
                 WordCount = 1;
             }
+        }
+
+        private bool IsValidRange(int limitedWordCount)
+        {
+            return limitedWordCount >= 1 && limitedWordCount <= WordCount;
+        }
+
+        private string[] GetLimitedWords(string source, string separator, int limitedWordCount)
+        {
+            if (limitedWordCount == 1)
+            {
+                return new[] {source};
+            }
+            else
+            {
+                var words = new string[limitedWordCount];
+                var splits = source.Split(new[] {separator}, StringSplitOptions.None);
+                for (var i = 0; i < limitedWordCount - 1; i++) words[i] = Words[i];
+                var skipCount = GetSkipCount(splits, limitedWordCount);
+                words[limitedWordCount - 1] = string.Join(separator, splits.Skip(skipCount));
+                return words;
+            }
+            
+        }
+
+        private int GetSkipCount(string[] splits, int limitedWordCount)
+        {
+            var nonEmptyWordCount = 0;
+            for (var i = 0; i < splits.Length; i++)
+            {
+                if (splits[i] == string.Empty) continue;
+                nonEmptyWordCount++;
+                if (nonEmptyWordCount == limitedWordCount) return i;
+            }
+            return 0;
         }
     }
 }
